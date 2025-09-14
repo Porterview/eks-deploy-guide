8f1f5a7a8ce486367c8f1015afa2d862317bcf12
Dustin Link <dustin@gossamerlabs.com>
2025-09-14 13:24:47 -0400

    add k8s provider + add external auth for gh-actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,16 @@ provider "aws" {
 provider "port" {
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
+}
+
 # Filter out local zones, which are not currently supported 
 # with managed node groups
 data "aws_availability_zones" "available" {
@@ -90,6 +100,27 @@ module "eks" {
       desired_size = 1
     }
   }
+}
+
+
+# Allow external deployers into the cluster via aws-auth ConfigMap
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = "arn:aws:iam::327207168534:role/k8s-deployers-gha"
+        username = "github-deployer"
+        groups   = ["system:masters"] # or a narrower group if you prefer
+      }
+    ])
+  }
+
+  depends_on = [module.eks]
 }
 
 
