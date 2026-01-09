a80f9c0a07954251cf6ef8c40bd6ac7c58c92b02
Dustin Link <dustinl@port.io>
2026-01-08 19:53:31 -0500

    update platform-operations role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,9 +21,9 @@ variable "port_run_id" {
   default     = ""
 }
 
-# ideally default this to null and define elsewhere
+# NOTE: ideally default this to null and define elsewhere
 variable "platform_engineers_role_arn" {
-  description = "IAM role ARN in 327207168534 assumed by Identity Center group 'platform-engineers'"
+  description = "IAM role ARN in 496973234569 assumed by Identity Center group 'platform-engineers'"
   type        = string
-  default     = "arn:aws:iam::327207168534:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_platform-operations_2677084c3e95224d"
+  default     = "arn:aws:iam::496973234569:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_platform-operations_cebcc15b810c4dd2"
 }
